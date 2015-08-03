18a8f0f177229fbed9bfe5e7d1b5bed0973e7d86
Alexander DeRieux <acd1797@vt.edu>
2015-08-02 22:23:16 -0400

    New method for signaling new entries from handler.

--- app/src/main/java/pinkraptorproductions/fitx/interfaces/RetainedFragmentInteractionListener.java
+++ app/src/main/java/pinkraptorproductions/fitx/interfaces/RetainedFragmentInteractionListener.java
@@ -1,10 +1,13 @@
 package pinkraptorproductions.fitx.interfaces;
 
 
+import android.os.Bundle;
+
 /**
  * Created by lndsharkfury on 7/23/15.
  */
 public interface RetainedFragmentInteractionListener {
 
     public void onRand(int value);
+    public void newEntries(Bundle data);
 }
